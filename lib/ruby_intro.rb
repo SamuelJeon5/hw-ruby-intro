063a028d2adc365c1d23b76e72a20ad78415bceb
SamuelJeon5 <soulstar67@gmail.com>
2022-02-06 13:22:08 +0000

    HW1 commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,61 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  else
+    x = 0
+    arr.each { |a| x+=a }
+  end
+  return x
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    x = arr.sort.reverse
+    sum = x[0] + x[1]
+    return sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).to_a.each {|x| return true if (x[0]+x[1]) == n}
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  a = s.downcase
+  special = "?<>',?[]}{=-)(*&^%$#`~{}"
+  v = ["a", "e", "i", "o", "u"]
+  if v.include? a[0]
+    return false
+  elsif s.empty?
+    false
+  elsif special.include? a[0]
+    return false
+  else
+    true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  elsif s.match?(/[[:alpha:]]/)
+    false
+  elsif s.to_i % 4 == 0
+    return true
+  end
 end
 
 # Part 3
